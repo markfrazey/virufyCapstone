6cd5541349831a07f082b7097fd848d6b3824687
Augene Pak <ajp824@uw.edu>
2023-05-02 17:18:13 -0700

    Cleaned page up

--- src/Album.js
+++ src/Album.js
@@ -1,36 +1,24 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import TimelineImage from './timeline.png';
 import TimelineVideo from './videos/Timeline Selection Animation.mp4'
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
+      {'Copyright © Abiy Abahoy, Augene Pak, Carson Essabhoy, Mark Frazey, Nora Casey 2023'}
     </Typography>
   );
 }
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const theme = createTheme({
   palette: {
@@ -59,10 +47,13 @@ export default function Album() {
         </Toolbar>
       </AppBar>
       <main>
-        <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}>
+        <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6}}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="text.primary" gutterBottom>
               Virufy Cough Annotator
+            </Typography>
+            <Typography variant="p" align="center" color="text.secondary" paragraph>
+              Team COVID-67
             </Typography>
             <Typography variant="p" align="center" color="text.secondary" paragraph>
               Abiy Abahoy, Augene Pak, Carson Essabhoy, Mark Frazey, Nora Casey
@@ -98,13 +89,10 @@ export default function Album() {
             </CardContent>
           </Card>
         </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          <Typography>testy testy</Typography>
-        </Container>
       </main>
       {/* Footer */}
       <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
+        {/* <Typography variant="h6" align="center" gutterBottom>
           Footer
         </Typography>
         <Typography
@@ -114,7 +102,7 @@ export default function Album() {
           component="p"
         >
           Something here to give the footer a purpose!
-        </Typography>
+        </Typography> */}
         <Copyright />
       </Box>
       {/* End footer */}
